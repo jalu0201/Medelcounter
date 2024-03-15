f72846426f51708eb56ec34811b30ba0bfebf634
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-03-15 10:58:56 +0100

    Add project files.

---
+++ Medelcounter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Medelcounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in ett heltal:");
+            double summa1 = 0;
+            double summa2 = 0;
+            double antal = 0;
+            double svar = 0;
+
+            while (summa1 >= 0)
+            {
+                summa1 = Convert.ToInt32(Console.ReadLine());
+                summa2 = summa2 + summa1;
+                antal++;
+                svar = summa2 / antal;
+                Console.WriteLine("Medelvärdet är:");
+                Console.WriteLine(svar);
+                Console.WriteLine("Skriv in ett heltal:");
+            }
+        }
+    }
+}
